aa89bbf2bac17ff1b132054becc67c12410810ce
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-10-21 08:40:15 -0600

    added cloudinary

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dkg7sngv6/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
